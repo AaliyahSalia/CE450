4639ebd9d48e960464d191e9559885b873b21310
AaliyahSalia <101353473+AaliyahSalia@users.noreply.github.com>
2022-09-25 23:05:58 -0700

    Create digitCalculator.py

---
+++ digitCalculator.py
@@ -0,0 +1,9 @@
+def same_ord():
+    a = int(input("Please enter the first positive number: "))
+    b = int(input("Please enter the second positive number: "))
+    if len(str(a)) == len(str(b)):
+        print("True")
+    else: 
+        print("False")
+
+same_ord()
