b8e038f2f3c0ecb3191acf39b86985049c52e2ef
AaliyahSalia <101353473+AaliyahSalia@users.noreply.github.com>
2022-09-25 23:55:38 -0700

    Update amc.py

--- amc.py
+++ amc.py
@@ -1,1 +1,20 @@
+def amc(): 
+    for x in range(284):
+        sum1=1
+        sum2=1
+        for j in range(2,x):
+            if x % j == 0:
+                sum1+=j
+        for i in range(2,sum1):
+                
+            if sum1%i == 0:
+                sum2+=i
+        
+        if x==sum2:
+            print(x,sum1)
+        else:
+            pass
 
+if __name__ == "__main__":
+    r=amc()
+    
